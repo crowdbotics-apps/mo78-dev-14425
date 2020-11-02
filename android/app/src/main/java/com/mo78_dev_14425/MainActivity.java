f1fb271fed4f227c99761590ce2751bff3ed7ed2
Crowdbotics <team@crowdbotics.com>
2020-11-02 17:36:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mo78_dev_14425/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mo78_dev_14425;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "mo78_dev_14425";
+    }
+}
---
+++ ios/mo78_dev_14425Tests/mo78_dev_14425Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface mo78_dev_14425 : XCTestCase
+
+@end
+
+@implementation mo78_dev_14425
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
